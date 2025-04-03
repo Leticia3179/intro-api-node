4f171a7961cdfc329fb7d8afbdfd311f84d6e532
Leticia3179 <leticiasampaio170305@gmail.com>
2025-04-03 20:36:31 -0300

    03/04

--- src/controllers/contatos.js
+++ src/controllers/contatos.js
@@ -1,29 +1,88 @@
 const db = require('../database/connection');
 
 module.exports = {
-   async listarContatos (request, response) {
-  try{
-    return response.status(200).json({
+   async listarContatos(request, response) {
+      try {
+         return response.status(200).json({
 
-        sucesso: true,
-        mensagem:'lista de usuarios',
-        dados: null
+            sucesso: true,
+            mensagem: 'lista de contatos',
+            dados: null
 
-     })
+         })
       }
+      catch (error) {
+         return response.status(500).json({
+            sucesso: false,
+            mensagem: 'erro na listagem de Contato',
+            dados: error.message
+         })
+      }
+   },
 
-     catch (error){
-         
-     return response.status(500).json({
 
-        sucesso: false,
-        mensagem:'erro na listagem de usuarios',
-        dados: error.message
 
-     })
 
-     }
+   async cadastarContatos(request, response) {
+      try {
+         return response.status(200).json({
 
-    }
+            sucesso: true,
+            mensagem: 'lista de contatos',
+            dados: null
 
+         })
+      }
+      catch (error) {
+         return response.status(500).json({
+            sucesso: false,
+            mensagem: 'erro na listagem de Contato',
+            dados: error.message
+         })
+      }
+   },
+
+
+
+
+   async editarContatos(request, response) {
+      try {
+         return response.status(200).json({
+
+            sucesso: true,
+            mensagem: 'lista de contatos',
+            dados: null
+
+         })
+      }
+      catch (error) {
+         return response.status(500).json({
+            sucesso: false,
+            mensagem: 'erro na listagem de Contato',
+            dados: error.message
+         })
+      }
+   },
+
+
+
+
+   async apagarContatos(request, response) {
+      try {
+         return response.status(200).json({
+
+            sucesso: true,
+            mensagem: 'lista de contatos',
+            dados: null
+
+         })
+      }
+      catch (error) {
+         return response.status(500).json({
+            sucesso: false,
+            mensagem: 'erro na listagem de Contato',
+            dados: error.message
+         })
+      }
+   },
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,22 @@ const express = require('express')
 const router = express.Router();
 
 const contatosController= require ('../controllers/contatos');
+const contatosController= require ('../controllers/usuarios');
 
 router.get('/contatos', contatosController.listarContatos);
+router.post('/contatos', contatosController.cadastarContatos);
+router.patch('/contatos', contatosController.editarContatos);
+router.delete('/contatos', contatosController.apagarContatos);
+
+
+
+
+router.get('/usuarios', contatosController.listarContatos);
+router.post('/usuarios', contatosController.cadastarContatos);
+router.patch('/usuarios', contatosController.editarContatos);
+router.delete('/usuarios', contatosController.apagarContatos);
+
+
+
 
 module.exports = router;
